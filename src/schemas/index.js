8007efda9b4ef526f17b3d8c4d80176c18275e53
beanc16 <bean.christopher@outlook.com>
2022-02-26 14:10:10 -0500

    Fix export bug

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,9 +1,11 @@
 const JoiRequired = require("./JoiRequired");
 const MongoDbSchemas = require("./mongodb");
+const MicroserviceSchemas = require("./microservices");
 
 
 
 module.exports = {
     JoiRequired,
     ...MongoDbSchemas,
+    ...MicroserviceSchemas,
 };
